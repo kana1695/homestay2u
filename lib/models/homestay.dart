5627e68e8c7c34249bd69a98f62c2677a2abc369
kana1695 <kanakashrisivam@gmail.com>
2026-06-11 23:35:01 +0800

    commit 5

--- lib/models/homestay.dart
+++ lib/models/homestay.dart
@@ -12,10 +12,6 @@ class Homestay {
   String? contactName;
   String? contactPhone;
   String? website;
-  String? latitude;
-  String? longitude;
-  String? source;
-  String? createdAt;
   String? imageUrl;
 
   Homestay.fromJson(Map<String, dynamic> json) {
@@ -43,10 +39,6 @@ class Homestay {
     contactName = json['contact_name']?.toString() ?? 'N/A';
     contactPhone = json['contact_phone']?.toString() ?? 'N/A';
     website = json['website']?.toString() ?? 'N/A';
-    latitude = json['latitude']?.toString() ?? 'N/A';
-    longitude = json['longitude']?.toString() ?? 'N/A';
-    source = json['source']?.toString() ?? 'N/A';
-    createdAt = json['created_at']?.toString() ?? 'N/A';
     imageUrl = json['image_url']?.toString();
   }
 }
